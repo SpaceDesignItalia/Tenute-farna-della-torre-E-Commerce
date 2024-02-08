abfa69834dc75fa74b1179b968e2556c59700162
Andrea Braia <andrix.braia@gmail.com>
2024-02-08 02:18:57 +0100

    Home and palette

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -102,6 +102,7 @@ export default function Login() {
                   <Button
                     onClick={handleSubmit}
                     color="primary"
+                    className="text-white"
                     radius="sm"
                     fullWidth
                   >
