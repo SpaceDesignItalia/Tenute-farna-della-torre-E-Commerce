a583e42275558016fd49fe86455d9df0ab1cd962
Francesco Roviello <frarovi04@gmail.com>
2024-06-04 12:29:23 +0200

    Fix pagina indirizzi grafica

--- src/API/API.js
+++ src/API/API.js
@@ -1,2 +1,1 @@
-/* export const API_URL = "https://api.tenutefarina.com:3000"; */
-export const API_URL = "https://localhost:3000";
+export const API_URL = "https://api.tenutefarina.com:3000";
---
+++ src/Components/Layout/CustomRadio.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useRadio, VisuallyHidden, cn } from "@nextui-org/react";
+
+export const CustomRadio = (props) => {
+  const {
+    Component,
+    children,
+    isSelected,
+    description,
+    getBaseProps,
+    getWrapperProps,
+    getInputProps,
+    getLabelProps,
+    getLabelWrapperProps,
+    getControlProps,
+  } = useRadio(props);
+
+  return (
+    <Component
+      {...getBaseProps()}
+      className={cn(
+        "group inline-flex items-center hover:opacity-90 active:opacity-50 justify-between flex-row-reverse tap-highlight-transparent",
+        "max-w-[350px] min-h-[165px] cursor-pointer border-2 border-default rounded-lg gap-4 p-4 my-3",
+        "data-[selected=true]:border-primary"
+      )}
+    >
+      <VisuallyHidden>
+        <input {...getInputProps()} />
+      </VisuallyHidden>
+      <span {...getWrapperProps()}>
+        <span {...getControlProps()} />
+      </span>
+      <div {...getLabelWrapperProps()}>
+        {children && <span {...getLabelProps()}>{children}</span>}
+        {description && (
+          <span className="text-small text-foreground opacity-70">
+            {description}
+          </span>
+        )}
+      </div>
+    </Component>
+  );
+};
